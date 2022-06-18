750231981d3636c19893aeda14c1db3068464710
Francisco Alface <falface@gmail.com>
2022-06-18 17:30:22 -0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Node application using docker-compose
+
+#### Node.js application using MySQL and Nginx.
+
+To run the application, run the following command `docker-compose up --build` and then open your browser on [localhost:8081](http://localhost:8081)
---
+++ node/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+const port = 3000;
+const config = {
+  host: "mysql",
+  database: "mydb",
+  user: "root",
+  password: "root",
+};
+
+app.get("/", async (req, res) => {
+  const connection = mysql.createConnection(config);
+  const sql = `INSERT INTO people(name) values('Francisco')`;
+  connection.query(sql);
+  connection.query("SELECT * FROM people", (err, response) => {
+    res.send(
+      `<h1>Full Cycle Rocks</h1> <br> <h2>${response.map(
+        (user) => `${user.name}`
+      )}</h2>`
+    );
+  });
+
+  connection.end();
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
